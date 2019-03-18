7286ff70e8847558ad5b14c37f0a6f798219e65b
Matthew Clapp <itsayellow+dev@gmail.com>
2019-03-18 16:02:03 -0700

    Quick check of implementing example network.

---
+++ keras_model.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+from keras.layers import Input
+from keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D
+from keras.layers import Dropout, Flatten, Dense
+from keras.models import Sequential, Model
+
+# layer                     output shape            params
+#------------------------------------------------------------
+# conv2d                    (None, 223, 223, 16)    208
+# maxPool2d                 (None, 111, 111, 16)    0
+# conv2d                    (None, 110, 110, 32)
+# maxPool2d                 (None, 55, 55, 32)
+# conv2d                    (None, 54, 54, 64)
+# maxPool2d                 (None, 27, 27, 64)
+# globalaveragepooling2d    (None, 64)
+# dense                     (None, 133)
+
+model1 = Sequential()
+model1.add(Conv2D(16, (2, 2), input_shape=(224, 224, 3)))
+model1.add(MaxPooling2D((2, 2), ))
+model1.add(Conv2D(32, (2, 2),))
+model1.add(MaxPooling2D((2, 2), ))
+model1.add(Conv2D(64, (2, 2),))
+model1.add(MaxPooling2D((2, 2), ))
+model1.add(GlobalAveragePooling2D())
+model1.add(Dense(133))
+# print summary of model1
+model1.summary()
+
+
+inputs = Input(shape=(224, 224, 3))
+x = Conv2D(16, (2, 2))(inputs)
+x = MaxPooling2D((2, 2))(x)
+x = Conv2D(32, (2, 2))(x)
+x = MaxPooling2D((2, 2))(x)
+x = Conv2D(64, (2, 2))(x)
+x = MaxPooling2D((2, 2))(x)
+x = GlobalAveragePooling2D()(x)
+predictions = Dense(133)(x)
+model2 = Model(inputs=inputs, outputs=predictions)
+# print summary of model2
+model2.summary()
